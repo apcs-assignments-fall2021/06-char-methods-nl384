1f6d447d211d0efce21d124b5f24742ab184ad9d
Nathan Lin <nlin@athenian.org>
2021-09-15 08:55:13 -0700

    first draft

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -4,24 +4,41 @@ public class MyMain {
     // If the char is not an uppercase letter, 
     // it is returned unchanged. 
     public static char toLower(char ch) {
-        // REPLACE WITH YOUR CODE
-        return '!';
+        if (ch < 'A' || ch > 'Z') {
+            return ch;
+        }
+        else {
+            return (char) (ch -'A' + 'a');
+        }
     }
 
     // Changes a lowercase letter to an uppercase letter
     // If the char is not a lowercase letter,
     // it is returned unchanged.
     public static char toUpper(char ch) {
-        // REPLACE WITH YOUR CODE HERE
-        return '!';
+        if (ch < 'a' || ch > 'z') {
+            return ch;
+        }
+        else {
+            return (char) (ch -'a' + 'A');
+        }
     }
 
     // The method is given a String as input and returns a
     // new String where all of the numbers have been removed
     // from the original String.
     public static String removeNumbers(String str) {
-        // REPLACE WITH YOUR CODE
-        return "!!!";
+        String newStr  = "";
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (c >= '0' && c <= '9') {
+                continue;
+            }
+            else {
+                newStr += c;
+            }
+        }
+        return newStr;
     }
 
     // Write some code to test your methods!
